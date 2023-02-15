9ed0e9b3881fc5c5168c12890989acb4d6d949d8
EmperorOwl <raylin9703@gmail.com>
2023-02-15 12:33:23 +1100

    Fix closing plot issue

--- backend/renderer.py
+++ backend/renderer.py
@@ -12,17 +12,19 @@ def render(tex: str):
     plt.rcParams['mathtext.fontset'] = 'cm'  # Computer Modern.
     plt.rcParams['font.family'] = 'DejaVu Serif'
     fig = plt.figure()  # Create new empty figure.
-    fig.text(
-        x=0,
-        y=0,
-        s=tex,
-        color='black'
-    )
-    plt.savefig(
-        fname=FNAME,
-        dpi=DPI,
-        bbox_inches='tight',
-        pad_inches=0.05,
-        transparent=False
-    )
-    plt.close()
+    try:
+        fig.text(
+            x=0,
+            y=0,
+            s=tex,
+            color='black'
+        )
+        plt.savefig(
+            fname=FNAME,
+            dpi=DPI,
+            bbox_inches='tight',
+            pad_inches=0.05,
+            transparent=False
+        )
+    finally:
+        plt.close()
